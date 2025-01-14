95f237f705646610951d76043cd96e31f2424e62
starmarket-sakvta <enyamdavaa528@gmail.com>
2025-01-14 21:12:30 +0800

    Update server.js

--- server.js
+++ server.js
@@ -48,10 +48,14 @@ app.post('/publish', async (req, res) => {
 
 // Connect to MongoDB
 mongoose
-  .connect('mongodb+srv://zonegamer528:emq1AD1Uesqk6r2w@star.kihsh.mongodb.net/?retryWrites=true&w=majority&appName=star', { 
+  .connect('mongodb+srv://zonegamer528:emq1AD1Uesqk6r2w@star.kihsh.mongodb.net/?retryWrites=true&w=majority&appName=star', {
     useNewUrlParser: true,
     useUnifiedTopology: true,
   })
   .then(() => {
     app.listen(3000, () => console.log('Server running on port 3000'));
   })
+  .catch((err) => {
+    console.error('Error connecting to MongoDB:', err);
+    process.exit(1); // Exit the application if the connection fails
+  });
