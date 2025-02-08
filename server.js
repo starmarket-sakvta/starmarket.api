6669f8d7cd4e91af10be70016c60fe83e68d688a
starmarket-sakvta <enyamdavaa528@gmail.com>
2025-02-08 22:13:21 +0800

    Update server.js

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require('express');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
 const cors = require('cors');
+const User = require("./models/User");
 
 const app = express();
 app.use(bodyParser.json());
