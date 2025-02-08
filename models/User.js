4e6a9ec75af5fd2f781e373b5688abe1e8765c75
starmarket-sakvta <enyamdavaa528@gmail.com>
2025-02-08 22:25:19 +0800

    Delete models directory

--- models/User.js
+++
@@ -1,8 +0,0 @@
-const mongoose = require("mongoose");
-
-const userSchema = new mongoose.Schema({
-    steamId: { type: String, required: true, unique: true },
-    balance: { type: Number, default: 0 }
-});
-
-module.exports = mongoose.model("User", userSchema);
